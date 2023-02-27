7e82735c859e3f7bdd5f08c63972008e6930a883
conache <C.conache@gmail.com>
2023-02-27 22:33:30 +0200

    Guess the new number

--- scripts/5-guess-the-random-number.js
+++ scripts/5-guess-the-random-number.js
@@ -14,6 +14,8 @@ async function main() {
 
   const val = await hackContract.getAnswer(blockBefore.hash, timestamp);
   await mainContract.connect(attacker).guess(val, { value: ethers.utils.parseEther("1.0") });
+
+  console.log("Is completed:", await mainContract.isComplete());
 }
 
 // We recommend this pattern to be able to use async/await everywhere
---
+++ scripts/6-guess-the-new-number.js
@@ -0,0 +1,22 @@
+const { ethers } = require("hardhat");
+
+async function main() {
+  const [deployer, attacker] = await hre.ethers.getSigners();
+  const GuessTheRandomNumberContract = await ethers.getContractFactory("GuessTheNewNumberChallenge", deployer);
+  const HackContract = await ethers.getContractFactory("GuessTheNewNumberChallengeHack", deployer);
+
+  const mainContract = await GuessTheRandomNumberContract.deploy({ value: ethers.utils.parseEther("1") });
+  const hackContract = await HackContract.deploy();
+
+  await hackContract.connect(attacker).attack(mainContract.address, { value: ethers.utils.parseEther("1.0") });
+  console.log("Is completed:", await mainContract.isComplete());
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
